05b2cccdbcb81572d3cf6cfd949a49b4e7eb1373
ndsunl <ndsunl@163.com>
2019-07-12 10:16:39 +0800

    day02_03

---
+++ python/python_100_days/day02_03.py
@@ -0,0 +1,18 @@
+"""
+使用 type() 检查变量的类型
+
+Version: 0.1
+Author: ndsunl
+Date: 2019-07-11
+"""
+
+a = 100
+b = 12.3456
+c = 1 + 5j
+d = "hello, world!"
+e = True
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
