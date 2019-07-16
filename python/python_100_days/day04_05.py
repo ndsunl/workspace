337a08ba68547419c96c311b499dcdafbbf5d618
ndsunl <ndsunl@163.com>
2019-07-16 13:40:37 +0800

    day04 2nd commit

--- python/python_100_days/day04_05.py
+++ python/python_100_days/day04_05.py
@@ -6,5 +6,7 @@ Author: ndsunl
 """
 
 for i in range(1, 10):
-    for j in range(1,i+1):
-        print(f"{i} * {j} = {i*j}")
+    for j in range(1, i+1):
+        print(f"{i}*{j}={i*j}", end='\t')
+    print()
+   
---
+++ python/python_100_days/day04_l1.py
@@ -0,0 +1,12 @@
+"""
+输入一个正整数判断它是不是素数
+
+Version: 0.1
+Author: ndsunl
+Date: 2019-07-16
+"""
+
+from math import sqrt
+
+num = int(input('请输入一个正整数: '))
+end = int(sqrt(num))
