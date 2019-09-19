bc0b4e4b50b451b5a16a21bb736f107e3366769f
ndsunl <ndsunl@163.com>
2019-09-19 17:42:26 +0800

    201909191742obox

---
+++ python/weixi/ex2.3类和对象加强练习.py
@@ -0,0 +1,46 @@
+"""
+类和对象的加强练习
+"""
+
+class CommonZombie:
+
+    def __init__(self):
+        self.healthPoint = 30
+        self.damagePoint = 5
+
+    def getHealthPoint(self):
+        if self.healthPoint > 0:
+            return self.healthPoint
+        else:
+            return "已经挂了！"
+
+    def getDamagePoint(self):
+        return self.damagePoint
+
+    def beHit(self, lostPoint):
+        self.healthPoint -= lostPoint
+        return self.lostPoint
+
+class PeaShooter:
+
+    def __init__(self):
+        self.healthPoint = 60
+        self.damagePoint = 5
+
+    def getHealthPoint(self):
+        if self.healthPoint > 0:
+            return self.healthPoint
+        else:
+            return "已经挂了"
+
+    def getDamagePoint(self):
+        return self.damagePoint
+
+z1 = CommonZombie()
+z2 = CommonZombie()
+p1 = PeaShooter()
+
+print("==当前状态==")
+print(f"僵尸z1 HP: {z1.getHealthPoint()}")
+print(f"僵尸z2 HP: {z2.getHealthPoint()}")
+print(f"豌豆射手p1 HP: {p1.getHealthPoint()}")
