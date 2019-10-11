c4b86d1b358c54761ddca835825e897b534d5331
ndsunl <ndsunl@163.com>
2019-10-11 17:29:10 +0800

    201910111729obox

--- python/spider/2017房价工资.py
+++ python/spider/2017房价工资.py
@@ -1,4 +1,7 @@
-import requests, bs4, re
+import requests
+import bs4
+import re
+import openpyxl
 
 
 def open_url(url):
@@ -18,18 +21,30 @@ def find_data(res):
     for each in target:
         if each.text.isnumeric():
             data.append([
-                re.search(r'\[(.+)\]',next(target).text).group(1),
-                next(target).text,
-                next(target).text,
-                next(target).text])
+                re.search(r'\[(.+)\]', next(target).text).group(1),
+                re.search(r'\d.*', next(target).text).group(),
+                re.search(r'\d.*', next(target).text).group(),
+                re.search(r'\d.*', next(target).text).group()])
 
     return data
+
+
+def to_excel(data):
+    wb = openpyxl.Workbook()
+    wb.guess_types = True
+    ws = wb.active
+    ws.append(['城市', '平均房价', '平均工资', '房价工资比'])
+    for each in data:
+        ws.append(each)
+
+    wb.save('2017年中国主要城市房价工资比排行榜.xlsx')
 
 
 def main():
     url = "https://news.house.qq.com/a/20170702/003985.htm"
     res = open_url(url)
-    print(find_data(res))
+    data = find_data(res)
+    to_excel(data)
 
 
 if __name__ == "__main__":
---
+++ python/spider/网易云音乐评论.py
@@ -0,0 +1,36 @@
+import requests
+import bs4
+
+
+def get_url(url):
+    headers = {
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+    res = requests.get(url, headers=headers)
+
+    params = 'OIFN8Rfs9xePiJaUFDt9DMbFT1wNADW0IoEFP2UMyc9ErwuOE/SJPxx/BF/46ttGd3UyYe6X7IhnfixKazKWucIkQSoeXOESBPxXnGHjS5HMqZVbWPybIF4cbZg8ItVog84oCTSZIxCJ/H2U0YH4/sxk81bHuRn556+31rVoujk6YgCajHtiUhlpNA1iRfvL'
+    encSecKey = '7d65cbc8b75413deeffdd086dd4f85300f80846ec82314ad4d87302a2d35f4e2c5fb8cf917f1dd687edd0dc3b8bbe18370cb8c99ee10f0f65ad72ddbfcec0e2c01cae6bbba624da189ebea430f20b19e3796577de86355c379c05b96c43149f04349a4302d23bf2817857e609c8eb455a2627fc7a3b2be13731b73f396cd9429'
+    data = {
+        'params': params,
+        'encSecKey': encSecKey}
+    target_url = 'https://music.163.com/weapi/v1/resource/comments/R_SO_4_4466775?csrf_token='
+
+    return res
+
+
+def get_data(res):
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    data = soup.find_all('img', class_="brand-tag brand-vip")
+
+    return data
+
+
+def main():
+    url = input('请输入链接地址：')
+    res = get_url(url)
+    data = get_data(res)
+
+    print(data)
+
+
+if __name__ == "__main__":
+    main()
