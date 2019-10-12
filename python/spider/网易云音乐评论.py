64b25feaf1652512952dfe9257bf42ea5f12c714
JackChen <ndsunl@163.com>
2019-10-12 13:53:00 +0800

    201910121352hbox

--- python/spider/网易云音乐评论.py
+++ python/spider/网易云音乐评论.py
@@ -1,10 +1,14 @@
 import requests
 import bs4
+import json
 
+def get_comments(url):
+    name_id = url.split('=')[1]
 
-def get_url(url):
     headers = {
-        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36'}
+        'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36',
+        'referer': 'https://music.163.com/song?id=4466775'}
+
     res = requests.get(url, headers=headers)
 
     params = 'OIFN8Rfs9xePiJaUFDt9DMbFT1wNADW0IoEFP2UMyc9ErwuOE/SJPxx/BF/46ttGd3UyYe6X7IhnfixKazKWucIkQSoeXOESBPxXnGHjS5HMqZVbWPybIF4cbZg8ItVog84oCTSZIxCJ/H2U0YH4/sxk81bHuRn556+31rVoujk6YgCajHtiUhlpNA1iRfvL'
@@ -12,20 +16,30 @@ def get_url(url):
     data = {
         'params': params,
         'encSecKey': encSecKey}
-    target_url = 'https://music.163.com/weapi/v1/resource/comments/R_SO_4_4466775?csrf_token='
+    
+    target_url = f'https://music.163.com/weapi/v1/resource/comments/R_SO_4_{name_id}?csrf_token='
 
     res = requests.post(target_url, headers=headers, data=data)
     
     return res
 
+def get_hot_comments(res):
+    comments_json = json.loads(res.text)
+    hot_comments = comments_json['hotComments']
+    
+    with open('hot_comments.txt', 'w', encoding='utf-8') as file:
+        for each in hot_comments:
+            file.write(each['user']['nickname'] + ':\n\n')
+            file.write(each['content'] + '\n')
+            file.write("------------------------------------\n")
+    #    file.close()
+
 
 def main():
     url = input('请输入链接地址：')
-    res = get_url(url)
+    res = get_comments(url)
+    get_hot_comments(res)
 
-    with open('comment.txt', 'w', encoding='utf-8') as file:
-        for each in file:
-            file.write(each)
 
 if __name__ == "__main__":
     main()
